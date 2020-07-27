1b08b0cc6f8009ee0c18dfd4fca60ea96ef2a0eb
fabiosouza <fabiosouza.web@gmail.com>
2020-07-27 20:54:01 -0300

    Finalizado Página. Pendente código para decifrar

--- code/chapter2.php
+++ code/chapter2.php
@@ -48,5 +48,23 @@
         <p>Qalqashandi defendia que a criptoanálise tem que conhecer a língua que foi escrita o criptograma. ele avaliou a frequência de letras do Corão e viu que era diferente de outros textos, então avaliou que para começar a avaliar um texto é preciso contar o número de letras e quantas vezes cada símbolo aparece, depois disso descobrir qual é o símbolo que separa as palavras, depois de ter encontrado o espaço, procure os símbolos que mais se repetem e suponha que seja as letras que mais se repetem, e depois o segundo símbolo que mais se repete e analisando se é possível construir palavras.</p>
 
         <p>Entretanto o método de Qalqashandi nao se espalharam rapidamente pelo mundo muçulmano, em 1600 o sultão de marrocos enviou seu secretário para negociar com a Elizabeth I da inglaterra, contra a espanha, mas o destinatário da mensagem levou 15 anos para entender a mensagem, mas se tiver utilizado o método de Qalqashandi levaria apenas algumas horas.</p>
+
+        <h5 style="color: #bf3fc8">Anagramantismo</h5>
+        
+        <p>Quando é deixado os espaços entre as palavras, facilita para localizar palavras com uma ou duas letras, sendo um bom início para decifrar, reconstruir o texto original desse jeito é chamado de anagramatizar.</p>
+
+        <p>Para criptografia é melhor não depender de um livro de códigos que pode ser capturado por inimigos, o melhor é decorar o método.</p>
+
+        <p>Um meio de criptografia é utilizar uma palavra chave, para reescrever a ordem do alfabeto e assim misturar a mensagem, por exemplo com a palavra ENGLAND, removemos as letras que se repetem e colocamos no alfabeto:</p>
+
+        <h6 style="color: #bf3fc8">Alfabeto comum:</h6>
+
+        <p>abcdefghijklmnopqrstuvwxyz</p>
+
+        <h6 style="color: #bf3fc8">Alfabeto criptografado:</h6>
+        
+        <p>ENGLADBCFHIJKMOPQRSTUVWXYZ</p>
+        
+        A palavra chave não precisa estar no início pode estar em qualquer lugar do alfabeto e apenas o destinatário e o remetente sabem qual é a palavra e a posição dela.
     </div>
 </div>
