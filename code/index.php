3c136f11e46a21494b83d7d99bd139a4a4437d62
fabiosouza <fabiosouza.web@gmail.com>
2020-08-05 19:05:36 -0300

    Feito página do capítulo 3

--- code/index.php
+++ code/index.php
@@ -154,7 +154,7 @@
             <!-- ======= Section Cap 3 ======= -->
             <section id="section-cap3">
                 <div class="container" data-aos="fade-up">
-                  <?php require_once('chapter3.php') ?>  
+                  <?php require_once('chapter3.php') ?>
                 </div>
             </section>
 
