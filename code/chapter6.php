c49e6612d59058478b238538eed7b98f04a8d72e
Elvis Leite da SIlva <38222579+elvis7t@users.noreply.github.com>
2020-08-07 19:48:11 -0300

    Entrega cap 6
    Entrega do cap 6 e ajuste do que faltava no 5

---
+++ code/chapter6.php
@@ -0,0 +1,160 @@
+<!-- Puzzle -->
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<div class="col-12"><br />  
+					<h5>Enigma</h5><br />
+				</div>
+				<div class="col-12 col-md-12">
+					<p class="text-justify"><strong>Decifre a mensagem cifrada por meio da tecnica transposi&ccedil;&atilde;o colunar</strong></p><br />
+					<p class="text-justify">Palavra chave: <strong>GUERRA</strong></p>
+					<p class="text-justify">Mensagen Transmitida: <strong>COZ SLA OFH BAL MDI EAT</strong></p>
+					<p class="text-justify">A mensagen foi embaralhada invertendo a ordem das colunas, tornando a palavra <strong>GUERRA</strong> em <strong>ARREUG</strong></p>
+					<p class="text-justify">Usando a transposição colunar encontre a mensagem original:</p>
+					
+					<div class="input-group mb-3" style="max-width: 500px">
+						<input type="text" class="form-control" placeholder="Mensagem Original" aria-label="Recipient's username" aria-describedby="button-addon2">
+						<div class="input-group-append">
+							<button class="btn btn-success" type="button" id="button-addon2">Verificar</button>
+						</div>
+					</div>				
+				</div>
+			</div>
+		</div>
+	</div>
+</div><br /> 
+<!-- Content -->		
+
+<div class="row">
+    <div class="col-12">
+        <h2 style="color: #39CCCC">O Nascimento da Mecanização</h2><br>
+		<p class="text-justify">A mecanização da criptografia começou no século XV com o disco de Alberti. Apesar de, Thomas Jefferson, terceiro presidente americano ter inventado outro sistema no século XVIII. </p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+		<h5>Discos dentro de discos</h5><br />        
+    </div>	
+	
+	<div class="col-12 col-lg-7">
+        <img src="view/static/images/thomas.jpg"  alt="Thomas Jefferson" class="img-fluid img-thumbnail">
+		<p>Thomas Jefferson</p>
+	</div><br />
+	<div class="col-12 col-md-12">
+		<p>Thomas Jefferson inventou um simples aparelho mecânico que codificava mensagens durante a Guerra da Independência Americana (1765-1783). Compostos por 36 discos de madeira com letras do alfabeto embaralhadas e com bordas de ordem aleatórias. Os discos eram todos diferentes e o remetente e o destinatário tinham que girá-los até que os eixos fossem alinhados para aparecer a linha da mensagem. </p>
+	</div>	
+	<div class="col-12 col-lg-7">
+        <img src="view/static/images/disco-jeferson.png"  alt="Os discos de Jefferson" class="img-fluid img-thumbnail">
+		<p>Os discos de Jefferson</p>
+	</div> 
+</div>
+
+<div class="row">	
+	<div class="col-12"><br />  
+        <h5>A Cifra de Patterson</h5><br />
+    </div>
+
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Jefferson não era adepto de máquinas de codificar, pois preferia utilizar uma nomenclatura (uma lista de nomes que podem ser substituídas por outros) inventada por Robert Livingston, secretário de Assuntos Exteriores dos EUA. Jefferson recomendava um sistema inventado por Robert Patterson, professor de Matemática da Universidade da Pensilvânia. O sistema era formado por uma coluna simples com letras nulas no alto das colunas, contando com mais letras nulas para preencher as linhas. Patterson julgava ser impossível decifrar o código, mesmo que fosse um conhecedor do sistema gera</p>
+	</div>
+</div>
+
+<div class="row">	
+	<div class="col-12"><br />  
+        <h5>Claude Chappe</h5><br />
+    </div>
+
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">O inventor Francês Claude Chappe, em 1792, apresentou seu primeiro sistema de telecomunicações da era industrial, o telégrafo óptico, que passou a fazer parte da paisagem da Europa durante quase cinquenta anos. A primeira mensagem oficial a ser enviada por este sistema relatou a recaptura da cidade de Le Quesnoy dos austríacos a prussianos. </p>
+	</div>
+	<div class="col-12 col-lg-7">
+		<img src="view/static/images/telegrama-optico.jpg"  alt="Telegrama Optico" class="img-fluid img-thumbnail"><br />	
+		<p>Telegrama Optico</p>
+	</div>
+</div>
+
+<div class="row">		
+	<div class="col-12"><br />  
+        <h5>A Cifra de Wadsworth</h5><br />
+    </div>
+	
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">O Coronel Decius Wadsworth, engenheiro e oficial da artilharia dos EUA, em 1817, desenvolveu um método mecânico de codificação similar ao de Alberti, compostos por dois discos. O disco externo tinha as 26 letras do alfabeto e os números 2 e 8. Eram gravados em pinos de latão que podiam ser montados em qualquer ordem. Já o disco interno era composto por 26 letras. Para que uma mensagem fosse codificada, acreditavam-se de que as letras do texto original eram soletreadas, uma de cada vez no disco interno, sendo equivalente ao disco externo para sua transmissão. </p>
+	</div>	
+</div>
+
+<div class="row">		
+	<div class="col-12"><br />  
+        <h5>Joseph Mauborgne (1881 – 1971)</h5><br />
+    </div>
+	
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Joseph Mauborgne dedicou a carreira a aprimorar a criptografia militar americana. Foi o pioneiro a solucionar a cifra de Playfair, usada pelos britânicos como cifra de campanha. </p>
+	</div>	
+</div>
+
+<div class="row">		
+	<div class="col-12"><br />  
+        <h5>Cilindro de Bazeries</h5><br />
+    </div>
+	
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Etienne Bazeries desenvolveu sua própria versão dos discos de códigos, as denominando Cilindro de Bazeries que fora utilizada pelo exército americano entre 1923 e 1942 e chamada de M-94. Uma medida proposta pelo capitão Parker Hitt. Hitt escreveu: “esse aparelho se baseia, até certo ponto, nas ideias do comandante Bazeries do exército Francês”. Ele simplesmente descolou os alfabetos embaralhados dos rotores do cilindro de Bazeries. Hitt também criou seu próprio cilindro, mas preferia o método das tiras. </p>
+	</div>	
+</div>
+
+<div class="row">	
+	<div class="col-12"><br />  
+        <h5>Conversa em Código</h5><br />
+    </div>
+
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">As forças armadas americanas usavam idiomas nativos e complexos como códigos militar na primeira guerra Mundial. Um grupo de índios chamados de choctaw, do estado  de Oklahoma foram os pioneiros no uso de idiomas nativos americanos como código militar. As forças armadas americanas colocavam choctaws (grupo étnico muscogueano nativo da região sudeste dos Estados Unidos da América) em todas as companhias e puderam transmitir mensagens mesmo que houvesse escuta no rádio ou nas linhas telefônicas.</p>
+	</div>
+	<div class="col-12 col-lg-7">
+		<img src="view/static/images/nacao-choctaw.png"  alt="Nação Choctaw" class="img-fluid img-thumbnail"><br />	
+		<p>Nação Choctaw</p>
+	</div>
+</div>	
+
+<div class="row">		
+	<div class="col-12"><br />  
+        <h5>Uma chave indecifrável</h5><br />
+    </div>
+	
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Após ajudar e projetar o M-94, Mauborgne desenvolveu uma chave indecifrável. Ele a criou enquanto testava um sistema de codificação automática, imaginado em 1918 por Gilbert Vernam, engenheiro da AT&T. Uma fita com o texto original passava pela máquina de codificação ao mesmo tempo em que outra fita contendo a chave. A mensagem, então seria codificada numa terceira fita, utilizando o que hoje chamaríamos de porta XOR. O segredo para decifrar a mensagem codificada, era passar novamente pela máquina utilizando a mesma chave. Mauborgne reconhecia que o ponto fraco do sistema de Vernam era que a chave girava o anel e percebeu que a solução seria uma fita-chave sem fim. </p>
+	</div>	
+</div>
+
+<div class="row">	
+	<div class="col-12"><br />  
+        <h5>A cifra de Wheatstone</h5><br />
+    </div>
+
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Em 1867 Wheatstone apresentou seu “Criptógrafo” na Exposição Universal de Paris. Semelhante a maquina de Wadsworth, com 26 letras no anel interno e 26 letras mais um espaço (totalizando 27 elementos) no anel externo, conhecido como “despacho”. Dois ponteiros como os de relógio eram ligados por engrenagens na proporção de 26:27. O ponteiro mais longo indicava as letras do anel externo, já o pequeno escolhia letras do anel interno. </p>
+	</div>
+	<div class="col-12 col-lg-7">
+		<img src="view/static/images/charles-wheastone.jpg"  alt="Charles Wheastone" class="img-fluid img-thumbnail"><br />	
+		<p>Charles Wheastone</p>
+	</div>
+</div>
+
+<div class="row">	
+	<div class="col-12"><br />  
+        <h5>Código Baudot</h5><br />
+    </div>
+
+	<div class="col-12 col-md-12">	
+		<p class="text-justify">Imaginado em 1874 pelo engenheiro francês Jean-Maurice-Émile Baudot, esse código tinha uma vantagem sobre o código Morse por incluir combinações de cinco sinais de corrente ligada ou desligada, com igual duração. Isso o tornava mais adequado para o uso em teletipos.  </p>
+	</div>
+	<div class="col-12 col-lg-7">
+		<img src="view/static/images/codigo-baudot.png"  alt="Código Baudot" class="img-fluid img-thumbnail"><br />	
+		<p>Código Baudot</p>
+	</div>
+</div>
+ 
+
