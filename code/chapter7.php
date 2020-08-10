85f64782a49a62007dd2d1a59335f119a3172ea6
fagnervalerio <fagner@fci.inf.br>
2020-08-10 13:48:15 -0300

    Capitulo 7

---
+++ code/chapter7.php
@@ -0,0 +1,180 @@
+<!-- Content -->		
+
+<div class="row">
+  <div class="col-12">
+    <h2 style="color: #39CCCC">A criação da Enigma</h2><br>
+    <p class="text-justify">
+      O engenheiro elétrico Arthur Scherbius, requereu em 1918, a patente de uma
+      máquina de codificação que usava rodas eletrificadas, alegando que sua máquina
+      evitaria a repetição de uma sequência de letras quando a mesma letra for pressionada
+      milhões de vezes, e ainda, que a solução de um telegrama também seria impossível se a
+      máquina cair em mãos não autorizadas, pois ela exige um sistema de chaves pré-
+      combinado.
+    </p>
+    <div class="text-center">
+      <img src="view/static/images/maquina_enigma.jpg"  alt="Máquina Enigma" class="img-fluid img-thumbnail"><br>
+      <small>Figura 1: Máquina Enigma</small>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12"><br />  
+      <h5>O Rotor</h5><br />
+  </div>
+  <div class="col-7">
+    <p class="text-justify">
+      O rotor utilizado na máquina de Scherbius, era um disco com 26 contatos cada lado, 
+      representando as letras do alfabeto. A fiação que ligava os discos era tal que o contato correspondente 
+      à letra “A” no lado da entrada estava ligado a “Y” no lado da saída.
+    </p>
+  </div>
+  <div class="col-5 mb-3 text-center">
+    <img src="view/static/images/rotor.jpg"  alt="Rotor" class="img-fluid img-thumbnail"><br>
+    <small>Figura 2: Rotor</small>
+  </div>  
+  <br/>
+  <div class="col-5 mb-3 text-center">
+    <img src="view/static/images/funcionamento_rotor.png"  alt="Funcionamento do Rotor" class="img-fluid img-thumbnail"><br>
+    <small>Figura 3: Funcionamento do Rotor</small>
+  </div>
+  <div class="col-7">
+    <p class="text-justify">
+      O texto original era inserido por meio de um teclado que enviava uma pequena corrente elétrica a uma placa de entrada 
+      que fazia contato com um dos lados do rotor, já o outro lado fazia contato com uma placa de saída, passando corrente 
+      a um bulbo que iluminava uma letra numa tela de vidro.
+    </p>
+  </div>
+  <div class="col-12 mb-3">
+    <p class="text-justify">
+      Caso o motor não se movesse, uma cifra simples de substituição monoalfabética seria obtida com a fiação da roda.
+      A batida na tecla da máquina de escrever girava o rotor uma letra à frente ou 1/26 de volta. 
+      As conexões dentro do rotor seriam diferentes, e uma letra diferente seria iluminada:
+    </p>
+    <div class="text-center">
+      <img src="view/static/images/teclado.png"  alt="Teclado" class="img-fluid img-thumbnail"><br>
+      <small>Se a mesma tecla fosse pressionada 26 vezes, a saída voltaria ao início</small>
+    </div>
+  </div>
+  <div class="col-9">
+    <p class="text-justify">
+      Com o acréscimo de outro rotor igual, a máquina passaria por 26x26 = 676 ciclos, antes de voltar ao início. 
+      Com 03 (três) rotores iguais, seriam 17.576 ciclos. Já com 04 (quatro) rotores: 456.976 ciclos e com 05 (cinco) 
+      rotores em 11.881.376 ciclos.
+    </p>
+    <p class="text-justify">
+      O destinatário teria que conhecer a posição inicial para decifrar o código, chamada “chave”, o que se tornava 
+      difícil já que existem 26 posições para cada rotor.
+    </p>
+    <p class="text-justify">
+      A máquina oferecida à marinha alemã, possuía 10 rotores por 4.000 a 5.000 marcos (560 a 720 dólares na época) 
+      e sua entrega seria em oito semanas. Sem êxito, Scherbius buscou caminhos comerciais para sua invenção, 
+      como em 1923 no congresso da União Postal Internacional, onde apresentou sua máquina, denominada: ENIGMA, 
+      que imprimia a mensagem com 38 cm de altura e pesava mais de 45 kg.
+    </p>    
+    <p class="text-justify">
+      Logo surgiu uma versão de apenas 11 cm e peso de 7 kg, tinha quatro rotores com controles dentados saindo da tampa 
+      para fazer os ajustes iniciais. Havia três filas de teclas de máquina de escrever para inserir o texto original e três 
+      linhas de letras em janelas circulares acima, com lâmpadas atrás para mostrar o texto cifrado, uma letra de cada vez.
+    </p>
+    <p class="text-justify">
+      Posteriormente os rotores se tornaram destacáveis, para que a ordem pudesse ser trocada. Um “refletor” substituía o 
+      quarto rotor, que não se movia e tinha contatos só de um lado, ligados em pares, levando corrente de volta para os 
+      três rotores móveis por um caminho diferente. Isso possibilitou cifrar e decifrar sem mudar a configuração, por outro lado, 
+      tornava uma letra original em outra, e essa outra na original, fazendo com que nenhuma letra pudesse ser cifrada como ela mesmo.
+    </p>
+  </div>
+  <div class="col-3 text-center">
+    <img src="view/static/images/arthur_scherbius.jpg"  alt="Arthur Scherbius" class="img-fluid img-thumbnail"><br>
+    <small>Figura 4: Arthur Scherbius</small>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12"><br />  
+      <h5>Elucidação</h5><br />
+  </div>
+  <div class="col-12">
+    <p class="text-justify">
+      A máquina Enigma tinha uma boa publicidade, mas não tinha boas vendas. Em 1919, o ex Primeiro Lorde do Mar John Fisher, 
+      mencionou que a “elucidação” das cifras do inimigo foi uma das maiores glórias do Almirantado.
+    </p>
+    <p class="text-justify">
+      Em 1925, Scherbius juntamente com E. Richard Ritter, criou a empresa Cipher Machine Stock Corporation, fornecendo máquinas 
+      Enigma para a marinha alemã. Essas versões possuíam teclado alfabético, tinham 29 contatos em cada roda, pois os livros de 
+      códigos navais utilizavam as letras tremadas: ä, ö e ü. Mesmo utilizando 03 rotores, a máquina era fornecida com 05, para 
+      mais possibilidades para criptografar. Evitando repetição, um livreto disponibilizava as formulações, que eram utilizadas 
+      através de envio como um grupo de letras que depois era decifrado. Apenas oficiais tinham permissão de configurar a posição 
+      dos rotores. O exército alemão exigiu mudanças, como um teclado padrão alemão, com o QWERTZUIO na fila superior de teclas.
+    </p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12"><br />  
+      <h5>O Painel de Conexão</h5><br />
+  </div>
+  <div class="col-12">
+    <p class="text-justify">
+      O exército alemão utilizou por pouco tempo a máquina com 8 rotores, optando pela instalação de um painel de conexão com 26 
+      soquetes entre o teclado e o primeiro rotor, pequenos cabos com plugues em cada ponta ligavam os pares.
+    </p>
+    <code>
+      Se A ligado a T <i class="icofont-arrow-right"></i> T é enviado, enquanto T é enviado como A
+    </code>
+    <br/><br/>
+    <p class="text-justify">
+      Em 1930, a marinha alemã aceitou a máquina do exército, impondo acréscimo de rotores.
+    </p>
+    <ul>
+      <li>Rotores I, II e III para comunicação com o exército (giravam para a esquerda uma letra apenas a cada giro);</li>
+      <li>Rotores IV e V guardados como reserva;</li>
+      <li>Rotores VI e VII para comunicação interna (tinham um segundo dente junto ao H e ao U que girava o rotor mais um espaço à esquerda).</li>
+    </ul>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12"><br />  
+      <h5>Procedimentos Padronizados</h5><br />
+  </div>
+  <div class="col-8">
+    <p class="text-justify">
+      Antes de codificar uma mensagem, o operador movia os rotores na posição inicial combinadas, conforme um livro 
+      de códigos que listava a chave de cada dia. Em seguida, teclava a mensagem letra a letra, anotando as letras 
+      que se iluminassem no painel. Por fim, o texto cifrado era entregue ao operador de rádio que a transmitia em código Morse. 
+      O recebimento das mensagens, sofria o processo inverso.
+    </p>
+    <p class="text-justify">
+      Com a opção de remoção para substituição dos 3 rotores, os alemães aumentavam 6 vezes a probabilidade (105.456) 
+      de a mensagem ser decifrada, só o painel de conexão que trocava os pares de seis das 26 letras, acrescentava mais 
+      100.391.791.500 de possibilidades. 
+      Totalizando 10.000.000.000.000.000 possíveis permutações dos ajustes dos rotores, bem como do painel de conexão, 
+      o que levaria mais tempo do que a idade do universo para decifrá-lo.
+    </p>
+    <p class="text-justify">
+      Gerando um nível maior de segurança nas comunicações, as forças armadas compraram mais de 30 mil máquinas Enigma, 
+      o que mais tarde acredit ter sido um papel importantíssimo em sua vitória na Segunda Guerra Mundial.
+    </p>
+  </div>
+  <div class="col-4 text-center">
+    <img src="view/static/images/batalha_franca.jpg"  alt="Enigma em uso na batalha da França" class="img-fluid img-thumbnail"><br>
+    <small>Figura 5: Enigma em uso na batalha da França</small>
+  </div>
+</div>
+
+<!-- Puzzle -->
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<div class="col-12"><br />  
+					<h5>Enigma</h5><br />
+				</div>
+				<div class="col-12 col-md-12">
+								
+				</div>
+			</div>
+		</div>
+	</div>
+</div><br /> 
