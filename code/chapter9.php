1a719f0ba9964d1ee4b3302b3ff0bf35685ccc8d
Elvis Leite da SIlva <38222579+elvis7t@users.noreply.github.com>
2020-08-24 22:06:09 -0300

    Menu e cap 9

---
+++ code/chapter9.php
@@ -0,0 +1,92 @@
+<!-- Content -->	
+<div class="row">
+    <div class="col-12">
+        <h2 style="color: #ce3c41">Escola de Cifras</h2><br>		
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12 col-md-8">        
+        <p class="text-justify">Criada em 1919, no inicio sobre os cuidados do almirantado e depois para o ministerio do exterior britanico. nas vesperas das 2 guerra cerca de 90 pessoas trabalhavam no local, sendo 30 criptografos e o restante do exercicito, os mesmos sendo resposaveis por capturar as mensagens.</p>
+		<p class="text-justify">A itália era considerada um perigo, pois seu ditador Mussolini começou a chamar o nosso mar mediterraneo, sendo a rota principal para a india, em 1935 a italia invadiu a abissinia ameacando o dominio britanico sobre o Egito. Os britanicos perceberam que os Italianos estavam usando a engima comercial, e para guebrar o codigo precisavam da ordem dos rotores da maquina e a chave diaria. Os britanicos ja sabiam que as letras nao podiam se transformar nela mesmo, com isso a maquina engima decifrava A como G a cifra tinha que ser G virar A.</p>		
+    </div>
+
+    <div class="col-12 col-md-4">
+	   <h5 align="center">Máquina Enigma</h5><br />
+		<img src="view/static/images/enigma-2.png"  alt="Telegrama Optico" class="img-fluid img-thumbnail"><br />			
+	</div>
+	
+	
+	<div class="col-12 col-md-12"> 
+		<p class="text-justify">A marinha italiana comecou a utlizar a enigma militar, então foi empregado um grupo de moças para realizar as trocas manuais e sugerido para procurarem o termo PER seguido por espaço durante meses nao foi decifrada nenhuma mensagem, mas uma estudande de alemão testou com personale, com esse texto cifrado foi o suficiente para decifrar o restante da mensagem e as outras mensagens do dia, alem de dar dicas sobre as cifras dos outros dias.</p>
+		<p class="text-justify">(pesquisar no google: bletchley park, tem interação)</p>
+		<p class="text-justify">Bletchley Park</p>
+		<p class="text-justify">Na região rural da inglaterra, protegido de bombardeios, local perfeito para esconder a entidade secreta  conhecida como Estação X, recrutando matematicos, linguistas, enxadristas e solucionadores de palavras cruzadas.</p><br>
+		
+		<h5>Mais matemáticos</h5><br>
+		
+		<p class="text-justify">o Diretor Alatair Denniston, percebeu que na nova era mecanizada a decifraçao precisava de matematicos, um dos primeiros foram Alan Turing, prodigio de 27 anos, depois de um curso de criptologia foi trabalhar para decifrar a enigma. Turing apaixonado pela teorica  matematica construiu uma maquina eletrica de multiplicar no doutorado.</p>
+		<p class="text-justify">no dia 10 de março de 1940 o exército alemao trocou o codigo da criptografia, uma semana depois “Vitoria” a maquina do Turing que emulava maquinas enigmas para testar as possibilidades de palavras chaves cada teste levava cerca de uma semana da maquina processando.</p><br>	
+	</div>
+</div>
+
+<div class="row">	
+	<div class="col-12">
+		<h5>Tabuleiro diagonal</h5><br />        
+    </div>	
+	
+	<div class="col-12 col-lg-7">	
+		<img src="view/static/images/tabuleiro-diagonal.png"  alt="Telegrama Optico" class="img-fluid img-thumbnail"><br />			    
+		
+		<p>Tabuleiro diagonal</p>
+	</div>
+	
+	<div class="col-12 col-md-12">	
+		 <p class="text-justify">Gordon Welchman, matematico que pensou em um meio de acelerar o processo, o Tabuleiro diagonal, produzindo um tabuleiro 676 contatos marcados com A a Z e entao a ligar todas as combinaçoes reciprocas possiveis, pois quando R era testada como D, o D precisava se tornar R, com isso reduzindo o numero de paradas erradas. Turing percebeu que isso permitia realizar testes simultaneos.</p>
+         <p class="text-justify">Durante toda a batalha da gra-bretania de julho de 1940 a equipe de Bletchley Park decifrou os codigos da equipe aerea alema e passou as informaçoes para os caças, entretando decifrar o codigo da marinha alema estva muito lento e os submarinos estavam atacando navios aliados privando a ilha de recursos como alimento e munição.</p><br />                 
+	</div>
+</div>
+
+<div>
+
+	<h5>Codigos da marinha.</h5><br />
+
+	<p class="text-justify">A marinha adicionou rotores a enigma, aumentando a ordem possivel de 60! para 336! e ainda implementaram a supercifraçao. O operador alemao escolhia 3 letras ao acaso cifrava ela duas vezes e para disfarçar melhor era adicionadas mais duas letras aleatorias no codigo, sem se abater, Alan turing e a cabana 8 trabalharam na enigma da marinha.</p>
+	<p class="text-justify">Seu auxiliar Hugh Alexander disse que nos primeiro dias apenas Turing achava que valia a pena atacar o problema.</p>
+	<p class="text-justify">Muitos avanços aconteceram quando maquinas e livros de códigos alemaes eram capturados, no dia 26 de abril foi interceptado um navio com bandeira holandesa carrengando munição, um tripulante viu uma sacola na agua boiando e nela foi encontrado o livro de registro do operador contendo mensagens cifradas com isso a equipe de Turing decifrar codigos de 6 dias.</p>
+	<p class="text-justify">No dia 4 de março de 1941 um navio alemao foi capturado com as configuraçoes do mes de fevereiro, embora desatualizado foram uteis para as atualizaçoes na enigma. com a decifração dos codigos notaram que tinham navios meterologicos alemaes que poderiam ter maquinas engmas, o palpite estava certo e encontraram os codigos do mes seguinte.</p>
+</div>
+
+
+<!-- Puzzle -->
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<div class="col-12"><br /> 
+					<h5>Enigma</h5><br />	
+				</div>	
+				<div class="col-12 col-md-12">
+					<p class="font-weight-bold">Decifre o código Morse observando a matriz abaixo.</p>				                					
+					<p class="text-justify">Mensagem escrita com Código Morse:</p>				
+					<img src="view/static/images/mensagem_boudot.png" alt="Morse" class="img-thumbnail rounded"><br />
+					
+					<br/><p class="text-justify"><strong>Traduza a mensagem:</strong></p>				
+					
+					<div class="input-group mb-3" style="max-width: 500px">				
+						<input type="text" class="form-control" placeholder="Mensagem Traduzida" aria-label="Recipient's username" aria-describedby="button-addon2">
+						<div class="input-group-append">
+							<button class="btn btn-success" onclick="return checkPasswordChapter(9, this)" type="button" id="button-addon2">Verificar</button>
+						</div>
+					</div>					
+				</div>
+				<div class="col-12 col-md-12">			
+					<img class="img-fluid img-thumbnail align-certer rounded" src="view/static/images/tabela-codigo-baudot.jpeg" alt="C&oacute;digo Baudot">			
+					<p>C&oacute;digo Baudot</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div><br /><br /> 
+ 
+
