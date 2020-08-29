6909c745cc49e2484082a63fdf31e460064c0fc3
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-29 20:46:39 -0300

    Adiciona botão de pular enigma nos caps 1 ao 15

--- code/chapter12.php
+++ code/chapter12.php
@@ -120,6 +120,7 @@
                     <input type="text" class="form-control" placeholder="Mensagem Traduzida" aria-label="Mensagem Traduzida" aria-describedby="button-addon2">
                     <div class="input-group-append">
                         <button class="btn btn-success" onclick="return checkPasswordChapter(13, this)" type="button" id="button-addon2">Verificar</button>
+                        <button class="btn btn-info" onclick="return skipOnePuzzle(13, this)" type="button" id="button-addon3" title="Pular desafio"> > </button>
                     </div>
                 </div>
             </div>
